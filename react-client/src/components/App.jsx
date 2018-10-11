414fe844e3116225abe3ab736b3e16a6e51b97d1
roman <roman.kozhukhivskyi@digitalcareerinstitute.org>
2018-10-11 11:47:40 +0200

    added scss

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import WarningMessage from "./WarningMessage";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="component-app">
-        hello world
-      </div>
+      <React.Fragment>
+        <Header />
+        <div className="component-app">
+          hello world
+        </div>
+        <WarningMessage />
+        <WarningMessage />
+        <WarningMessage />
+        <WarningMessage />
+      </React.Fragment>
     );
   }
 }
---
+++ react-client/src/components/WarningMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class WarningMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Warning Message!
+      </div>
+    );
+  }
+}
+export default WarningMessage;
